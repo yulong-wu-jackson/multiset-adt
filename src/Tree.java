15acd7a5b498d9f6b124295d827dce18d585b430
Yulong <jackson.yl.wu@outlook.com>
2023-09-20 22:48:19 -0400

    constructor of tree

--- src/Tree.java
+++ src/Tree.java
@@ -1,2 +1,11 @@
+import java.lang.reflect.Array;
+
 public class Tree {
+    private Tree root;
+    private Tree[] subtrees;
+    public Tree(Tree root, Tree[] subtrees){
+        this.root = root;
+        this.subtrees = subtrees;
+
+    }
 }
